462f70cb65c1ed032802da164caea6b0c2095efc
sestivi7 <kyb6079@naver.com>
2023-08-25 17:28:43 +0900

    main page

--- dist/js/slide.js
+++ dist/js/slide.js
@@ -1,6 +1,13 @@
-var splide = new Splide( '.splide', {
-  type   : 'loop',
+var splide = new Splide(".splide", {
+  type: "loop",
   // padding: '5rem',
-} );
+});
+
+splide.mount();
+
+var splide = new Splide(".splide-tiket", {
+  type: "loop",
+  // padding: '5rem',
+});
 
 splide.mount();
