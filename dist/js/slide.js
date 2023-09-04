5313ff0662b75dede06481ba09523933a9bc73d9
sestivi7 <kyb6079@naver.com>
2023-09-04 17:49:45 +0900

    main

--- dist/js/slide.js
+++ dist/js/slide.js
@@ -5,9 +5,22 @@ var splide = new Splide(".splide", {
 
 splide.mount();
 
+// var splide = new Splide(".splide-tiket", {
+//   type: "loop",
+//   // padding: '5rem',
+// });
+
+// splide.mount();
+
+// option
+// https://splidejs.com/guides/options/#type
+
 var splide = new Splide(".splide-tiket", {
-  type: "loop",
-  // padding: '5rem',
+  type: "slide",
+  // perPage: 2,
+  padding: "5rem",
+  gap: "10px",
+  start: 1,
 });
 
 splide.mount();
